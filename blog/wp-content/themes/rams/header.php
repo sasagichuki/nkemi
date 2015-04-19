7342e01b8c28db4f56bf3aeecc10f949e2cf7f0f
Charles Gichuki <charles@sprout.co.ke>
2015-04-19 13:18:08 +0300

    added Kenchic image Removed Mac comestics bg

--- blog/wp-content/themes/rams/header.php
+++ blog/wp-content/themes/rams/header.php
@@ -75,7 +75,7 @@
 					
 				 </ul>
 				 
-				 <p class="credits"><?php _e('Theme by','rams'); ?> <a href="http://www.andersnoren.se">Anders Nor&eacute;n</a></p>
+				 <p class="credits"><a href="http://www.nkemi.co.ke">Nkemi Consulting</a></p>
 				
 				 <div class="clear"></div>
 			
